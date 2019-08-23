f54fa623d1a3bb47904487f050f8bd128d69c2a4
emzarts <17zimmermane@gmail.com>
2019-08-22 21:02:37 -0400

    Updates

---
+++ precache-manifest.908271a7b5c6dcc188973528be35db2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ad59dd75641d943178ff2bebdab0129",
+    "url": "/emzarts/mood-tracker/index.html"
+  },
+  {
+    "revision": "6c5c01f192bcf97a4d71",
+    "url": "/emzarts/mood-tracker/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "0bb906dbf33027e4960a",
+    "url": "/emzarts/mood-tracker/static/js/2.7366d528.chunk.js"
+  },
+  {
+    "revision": "6c5c01f192bcf97a4d71",
+    "url": "/emzarts/mood-tracker/static/js/main.daee1fbc.chunk.js"
+  },
+  {
+    "revision": "8592f9fb77b6db4f30f7",
+    "url": "/emzarts/mood-tracker/static/js/runtime~main.cbee675a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/emzarts/mood-tracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
